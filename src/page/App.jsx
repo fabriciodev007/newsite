69f0f93d4807eafa2d476a6eff98a17ab95afe1c
fabricio lima <fabricio.tree@gmail.com>
2026-03-23 10:29:26 -0300

    repositorio teste

---
+++ src/page/App.jsx
@@ -0,0 +1,25 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div>
+      <h1 className="text-center text-primary">hello world</h1>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">My App</a>
+          <a className="navbar-brand" href="#">My App</a>
+          <a className="navbar-brand" href="#">My App</a>
+        </div>
+      </nav>
+    </div>
+    
+
+    </>
+  )
+}
+
+export default App
